8b3dc544474bc905fc460d2b589814389d6f212d
The Mini John <admin@phoenix-prime.gr>
2014-10-01 22:36:46 +0200

    Add Basic Styles

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,2 +1,16 @@
 class PlayersController < ApplicationController
+	before_action :set_player, only: [:show, :edit, :update, :destroy]
+
+	def index
+		@players = Player.all
+	end
+
+	def show
+		@player = Player.find(params[:id])
+	end
+
+	private
+	def set_player
+		@player = Player.find(params[:id])
+	end
 end
