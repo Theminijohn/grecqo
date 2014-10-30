b2b4b0fa3f85cd6dbd1680d6b38af9adc1679539
The Mini John <admin@phoenix-prime.gr>
2014-10-30 13:26:20 +0100

    Player Nav Tab + Googlez

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
   def home
+  	@search = Player.search(params[:q])
+		@players = @search.result.order("points desc").paginate(page: params[:page], per_page: 25)
   end
 end
