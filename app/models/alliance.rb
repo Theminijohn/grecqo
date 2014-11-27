220e6e0d78a66ac0691603faddd3c52c0cc36e1b
The Mini John <admin@phoenix-prime.gr>
2014-11-27 15:05:38 +0100

    Fixes

--- app/models/alliance.rb
+++ app/models/alliance.rb
@@ -1,12 +1,23 @@
 class Alliance < ActiveRecord::Base
 
-	self.primary_key =  'grepo_id'
+  self.primary_key =  'grepo_id'
 
-	# Associations
-	has_many :players
-	
-	# Params
-	def to_param
-		"#{self.id}-#{self.name.downcase.gsub(" ", "-")}"
-	end
+  # Associations
+  has_many :players
+  has_many :towns, through: :players
+  
+  # Params
+  def to_param
+    "#{self.id}-#{self.name.downcase.gsub(" ", "-")}"
+  end
+
+  # Ocean Percentages
+  def ocean_percentage
+    return @ocean_percentage if defined? @ocean_percentage 
+    ocean_town_counts = towns.group(:ocean).count 
+    total_cities = ocean_town_counts.values.sum.to_f 
+
+    @ocean_percentage = ocean_town_counts.map {|ocean, count| [ocean, (count / total_cities * 100).round(2)]}.to_h
+  end
+
 end
